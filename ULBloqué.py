7cc0ab9ebf21fea53fcf0f47a768d5d1141b6e05
Neritsusama <ainanathram@gmail.com>
2024-11-19 22:09:31 +0100

    parse_done

--- ULBloqué.py
+++ ULBloqué.py
@@ -1,18 +1,51 @@
 # initialiser le jeu
-def read_map(game_file):
+def parse_game(game_file_path):
+    if "+" in game_file_path :
+        data = game_file_path.strip().split("\n")
+    else:
+        with open(game_file_path, encoding="utf-8") as f:
+            data = f.readlines()
     game = {}
-    with open(game_file, encoding="utf-8") as f:
+    with open(game_file_path, encoding="utf-8") as f:
         data = f.readlines()
-        game["widht"]= len(data[0][])
-        game["height"] = len(data)
+        used_lines = [line.replace("|", "").strip() for line in data if '|' in line]
+        game["width"] = len(used_lines[0])  
+        game["height"] = len(used_lines)    
+        game["cars"] = get_cars_draft(used_lines)
+        game["max_moves"] = int(data[-1].strip()) 
 
-    return data, game["height"], game["widht"]
-print(read_map("game1.txt"))
+    return game
+
+def get_cars_draft(map):
+    cars_draft = {}
+    for y, line in enumerate(map):
+        for x, letter in enumerate(line):
+            if letter.isalpha():  
+                if letter not in cars_draft:
+                    cars_draft[letter] = {'positions': []}
+                cars_draft[letter]['positions'].append((x, y))
+    cars = []
+    for car, infos in sorted(cars_draft.items()):
+        positions = infos['positions']
+        size = len(positions)
+        if len(positions) > 1:
+            if all(x == positions[0][0] for x, y in positions):
+                orientation = 'v' 
+            else:
+                orientation = 'h' 
+        cars.append([positions[0], orientation, size])
+    return cars
+
+
+
+
+
 #file_game
 #affichage
-#séléction de la voiture
-#choix de la direction ou changement de de voiture
+#séléction de la cars
+#choix de la direction ou changement de de cars
 #Vérification 
 # Déplacement
 # Vérification de la victoire
 # Fin du jeu testtttt
+
